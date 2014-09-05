c03819989c78ab823d902d89358b3ba4b63a1a27
cottyard <cottyard@gmail.com>
2014-09-06 00:20:04 +0800

    refactor server.js

--- server.js
+++ server.js
@@ -1,58 +1,68 @@
 var libpath = require('path'),
-    http = require("http"),
+    http = require('http'),
     fs = require('fs'),
-    url = require("url"),
+    url = require('url'),
     mime = require('mime');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var pushStateDisabled = true;
 
-var requestHandler = function (request, response) {
+var request_handler = function (request, response) {
 
-    var uri = url.parse(request.url).pathname;
-    var filename = libpath.join('.', uri);
-
-    fs.exists(filename, function (exists) {
-        if (!exists) {
-            if( pushStateDisabled ) {
-                response.writeHead(404, {
-                    "Content-Type": "text/plain"
-                });
-                response.write("404 Not Found\n");
-                response.end();
-                return;
-            } else {
-                filename = libpath.join(path, '/index.html');
-            }
-        }
-
+    var respond_file_not_found = function(){
+        response.writeHead(404, {
+            "Content-Type": "text/plain"
+        });
+        response.write("404 Not Found\n");
+        response.end();
+    };
+    
+    var respond_server_error = function(err){
+        response.writeHead(500, {
+            "Content-Type": "text/plain"
+        });
+        response.write(err + "\n");
+        response.end();
+    };
+    
+    var respond_succeed = function(type, file){
+        response.writeHead(200, {
+            "Content-Type": type
+        });
+        response.write(file, "binary");
+        response.end();
+    };
+    
+    var send_file = function(filename) {
         if (fs.statSync(filename).isDirectory()) {
             filename += '/index.html';
         }
 
         fs.readFile(filename, "binary", function (err, file) {
             if (err) {
-                response.writeHead(500, {
-                    "Content-Type": "text/plain"
-                });
-                response.write(err + "\n");
-                response.end();
+                respond_server_error(err);
                 return;
             }
-
             var type = mime.lookup(filename);
-            response.writeHead(200, {
-                "Content-Type": type
-            });
-            response.write(file, "binary");
-            response.end();
+            respond_succeed(type, file);
+            return;
         });
+    };
+    
+    var uri = url.parse(request.url).pathname;
+    var filename = libpath.join('.', uri);
+    
+    fs.exists(filename, function (exists) {
+        if(!exists){
+            respond_file_not_found();
+            return;
+        }
+        send_file(filename);
     });
 };
 
 var server = http.createServer();
-server.addListener('request', requestHandler);
+server.addListener('request', request_handler);
 server.listen(port, ipaddress);
 
 console.log("Static file server running at\n" +
